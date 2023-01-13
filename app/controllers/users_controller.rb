b864a581403e2f9e7f1b9de76cf56a7510203c86
Kevin Dutzy <kddutz15@gmail.com>
2023-01-13 00:14:31 -0500

    commit tree 3acd9cfd4db85f7eb5ff6d29cec8b6c8a1208fd2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,14 @@ class UsersController < ApplicationController
 
   def show
     user = User.find_by(id: params[:id])
-    render json: user, include: :items
+      render json: user, include: :items
   end
+
+
+  # private
+
+  # def user_not_found
+  #   render json: { error: "User not found" }, status: :not_found
+  # end
 
 end
